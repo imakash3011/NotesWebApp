d9552cf30de6bed81f9e49d4ca134b654304d70c
imakash3011 <imakashpatel3011@gmail.com>
2020-09-19 13:58:26 +0530

    Multiple notes card added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
-console.log("Welcome to Notes app")
+console.log("Welcome to Notes app");
+showNotes();
 
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  // console.log("added notes Successfully")
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+  showNotes();
+});
 
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">          
+          <div class="card-body">
+        <h5 class="card-title">Note ${index + 1}</h5>
+        <p class="card-text">${element}</p>
+        <button href="#" class="btn btn-primary">Delete Note</button>
+      </div> 
+    </div> 
+    `;
+  });
+  let  notesElm = document.getElementById('notes');
+  if(notesObj.length != 0){
+    notesElm.innerHTML = html;
+  } else{
+    notesElm.innerHTML =`Nothing to Show`;
+  }
+}
